2e04620e6c171268ac24954f48e6fe48f10d7aa1
David F <davidfawzy02@gmail.com>
2023-11-17 15:27:11 -0500

    Added file name transfer before file content transfer for the server to organize the content appropriately (it was mixing the file contents between files as the file name list was not in order). Added a gitignore file for the IDE specific folder. Formatted code into camelCasing instead of snake_casing to maintain consistent variable naming conventions and made global variables at the top of the files (constants) to follow the constant naming convention (all uppercase). Removed format encoding/decoding. Might have to be implemented later.

--- client.py
+++ client.py
@@ -1,56 +1,33 @@
 import socket
 import os
-import random
-import string
 import base64
 
 IP = "127.0.0.1"
-port = 59000
-format = "utf-8"
-
-#Function to populate the localFolder with 20 files with at least 5kbs of sizes DONT CALL THIS FUNCTION AGAIN !
-def createFiles(folder_name):
-
-    os.makedirs(folder_name,exist_ok=True)
-
-    file_size = 5
-    number_of_files =20
-
-    for i in range(1,number_of_files+1):
-        file_name = f"file{i}.txt"
-        file_path = os.path.join(folder_name,file_name)
-
-        # Generate random text content
-        content = ''.join(random.choices(string.ascii_letters + string.digits, k=file_size * 1024))
-
-        with open(file_path,'w') as file:
-            file.write(content)
-
-def send_files(client_socket,folder_name):
-
-    files = os.listdir(folder_name) # Grab the contents of the file to send
-
-    for file_name in files:
-
-        data = open(file_name,'b')
-        
-        #file_path = os.path.join(folder_name,file_name)
-        #client_socket.send(file_path.encode(format))
-        
-        #file_path = os.path.join(folder_name,file_name) # create file path to send
-        #client_socket.send(file_path.encode())
-        #file = open(file_path,"r")
-        #data = file.read()
-        #client_socket.send(data.encode(format)) # send the file contents to the server
+PORT = 59000
+FOLDER_NAME = "localFolder"
+SERVER_BUFFERSIZE = 5120
 
 def main():
+    # Connect to server socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect( (IP, PORT) )
+    
+    # Get list of file names in folder
+    files = os.listdir(FOLDER_NAME) # Grab the contents of the file to send
 
-    client_folder = "localFolder"
-    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client.connect((IP,port))
-    send_files(client,client_folder)
+    for fileName in files:
+        # Pad the file name to make it the size of the server's buffersize
+        paddedFileName = (fileName + "\n").ljust(SERVER_BUFFERSIZE)
 
+        # Send to the server the file name
+        server.send( paddedFileName.encode() )
 
+        # Open each file in byte read mode
+        with open(os.path.join(FOLDER_NAME, fileName), "rb") as file:
+            # Send the file content to the server
+            print("Sending contents of " + fileName + " to server...")
+            fileContent = file.read()
+            server.sendall( fileContent )
 
-
-main()
+if __name__ == "__main__":
+    main()
