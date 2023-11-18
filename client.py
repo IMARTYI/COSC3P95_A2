3ac5a9e930d48dc3f0865e876776b2154663e35c
David F <davidfawzy02@gmail.com>
2023-11-18 09:47:44 -0500

    Added back format constant. Added constants into .env file. Make sure to pip install python-dotenv. Added varying size file transfer using a fixed header size that contains the file name and size first. Added ks.py script to kill the server. Simply run it to kill the server that way the server's infinite loop doesnt bug the terminal.

--- client.py
+++ client.py
@@ -1,11 +1,15 @@
 import socket
 import os
-import base64
+from dotenv import load_dotenv
 
-IP = "127.0.0.1"
-PORT = 59000
+load_dotenv()
+
+IP = os.getenv("IP")
+PORT = int( os.getenv("PORT") )
+NUM_OF_FILES = int( os.getenv("NUM_OF_FILES") )
+HEADER_SIZE = int( os.getenv("HEADER_SIZE") )
+FORMAT = os.getenv("FORMAT")
 FOLDER_NAME = "localFolder"
-SERVER_BUFFERSIZE = 5120
 
 def main():
     # Connect to server socket
@@ -16,17 +20,19 @@ def main():
     files = os.listdir(FOLDER_NAME) # Grab the contents of the file to send
 
     for fileName in files:
-        # Pad the file name to make it the size of the server's buffersize
-        paddedFileName = (fileName + "\n").ljust(SERVER_BUFFERSIZE)
-
-        # Send to the server the file name
-        server.send( paddedFileName.encode() )
-
         # Open each file in byte read mode
-        with open(os.path.join(FOLDER_NAME, fileName), "rb") as file:
+        with open(os.path.join(FOLDER_NAME, fileName), "r") as file:
+            fileContent = file.read().encode(FORMAT)
+            contentSize = len(fileContent)
+
+            # Pad the file name to make it the size of the server's buffersize
+            paddedHeader = (f"{fileName},{contentSize},").ljust(HEADER_SIZE)
+
+            # Send to the server the file name
+            server.sendall( paddedHeader.encode(FORMAT) )
+
             # Send the file content to the server
-            print("Sending contents of " + fileName + " to server...")
-            fileContent = file.read()
+            print(f"Sending contents of {fileName} to server...")
             server.sendall( fileContent )
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 import socket
-import keyboard
 import os
-import base64
 import threading
+from dotenv import load_dotenv
 
-IP = "127.0.0.1"
-PORT = 59000
-NUM_OF_FILES = 20
-BUFFERSIZE = 5120 # 5Kb
+load_dotenv()
+
+IP = os.getenv("IP")
+PORT = int( os.getenv("PORT") )
+NUM_OF_FILES = int( os.getenv("NUM_OF_FILES") )
+HEADER_SIZE = int( os.getenv("HEADER_SIZE") )
+FORMAT = os.getenv("FORMAT")
+
+exitServer = False
 
 def handleClient(clientSocket, address):
+    global exitServer
+
     # Init folder that will hold the client's files
     # Clears out the illegal characters in folder names like . and ' by replacing them away
     folderName = "Client " + str(address).replace(".", "_").replace("\'", "")
@@ -19,16 +25,27 @@ def handleClient(clientSocket, address):
 
     for i in range(NUM_OF_FILES):
         # No need to decode as we recieve as bytes and then write it later into the file as bytes
-        fileName = clientSocket.recv(BUFFERSIZE).decode().split("\n")[0]
-        fileContent = clientSocket.recv(BUFFERSIZE)
+        headerData = clientSocket.recv(HEADER_SIZE).decode(FORMAT).split(",")
+        fileName = headerData[0]
+        fileSize = int( headerData[1] )
+
+        if fileName == "KILL":
+            exitServer = True
+            os.rmdir(folderName)
+            break
+
+        fileContent = clientSocket.recv(fileSize)
 
         # Open file to write to
-        # fileName = "file" + str(i+1) + ".txt"
         with open(os.path.join(folderName, fileName), "wb") as file:
-            print("Creating " + fileName)
             file.write(fileContent)
+            print("Created " + fileName)
+        
+    clientSocket.close()
 
 def main():
+    global exitServer
+
     # Initialize socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind( (IP, PORT) )
@@ -36,13 +53,15 @@ def main():
     print("Server is listening on port", PORT)
     
     # Listen for client requests
-    while True:
+    while not exitServer:
         client, address = sock.accept()
         print("Got connection from", address)
         
         # Start a thread for each client
-        clientThread = threading.Thread(target=handleClient, args=(client, address,))
+        clientThread = threading.Thread(target=handleClient, args=(client, address))
         clientThread.start()
+    
+    sock.close()
 
 if __name__ == "__main__":
     main()
